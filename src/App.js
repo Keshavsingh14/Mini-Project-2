5545142dabcb1713353d16be119e27606529a8c9
Keshav Shakti Singh <keshavshakti14@gmail.com>
2023-04-14 20:06:53 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="6eeb0df4-549c-47d1-9b38-8684de6a613f"
+        userName="keshav1412"
+        userSecret="12345"
+        
+        />
+    );
 }
 
 export default App;
