b8f26dfdc5e085ba84579e003110b70e396b245c
Keshav Shakti Singh <keshavshakti14@gmail.com>
2023-04-24 19:06:03 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '6eeb0df4-549c-47d1-9b38-8684de6a613f';
+
 const App = () => {
-    return (
-        <ChatEngine
-        height="100vh"
-        projectID="6eeb0df4-549c-47d1-9b38-8684de6a613f"
-        userName="keshav1412"
-        userSecret="12345"
-        
-        />
-    );
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+
 
 export default App;
